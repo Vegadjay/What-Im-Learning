af96e6ddc8b87224ae5f4c65257b17091903ea0c
Jay Vegad <jayvegad10@gmail.com>
2024-07-20 11:00:36 +0530

    update list

---
+++ DSA/LinkList/CircularListInserAtFirst.java
@@ -0,0 +1,94 @@
+/*
+ * In this programme we can make CircularLinklist and impliment it from screatch and make it simple as possible as..
+ * In this programme i am adding inserAtfirst position, Last Position, Delete at First position, last position, and make more
+ * methods
+ * @author : Jay Vegad
+ * @date : 20-7-2024
+ */
+
+public class CircularListInserAtFirst {
+    class Node {
+        int data;
+        Node link;
+
+        Node(int data) {
+            this.data = data;
+            this.link = null;
+        }
+    }
+
+    private Node first = null;
+    private Node last = null;
+
+    // This method add node at first postion of the circular linkedlist
+
+    public void insertAtFirst(int data) {
+        Node newNode = new Node(data);
+
+        if (first == null) {
+            first = last = newNode;
+            newNode.link = newNode;
+        } else {
+            newNode.link = first;
+            last.link = newNode;
+            first = newNode;
+        }
+    }
+
+    // This method add Node at last position of the circular linkedlist
+
+    public void inserAtlast(int data) {
+        Node newNode = new Node(data);
+        if (first == null) {
+            first = last = newNode;
+            newNode.link = newNode;
+        } else {
+            newNode.link = first;
+            last.link = newNode;
+            last = newNode;
+        }
+    }
+
+    // This method insert Node at given index
+
+    public void inserAtOrder(int data) {
+        Node newNode = new Node(data);
+        Node save = null;
+        if (first == null) {
+            newNode.link = newNode;
+            first = last = newNode;
+            return;
+        }
+        if (newNode.data <= first.data) {
+            newNode.link = first;
+            last.link = newNode;
+            first = newNode;
+        }
+        save = first;
+
+        while (save != last && newNode.data >= save.link.data) {
+            save = save.link;
+        }
+
+        newNode.link = save.link;
+        save.link = newNode;
+
+        if (save == last) {
+            last = newNode;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        CircularListInserAtFirst list = new CircularListInserAtFirst();
+        list.insertAtFirst(10);
+        list.insertAtFirst(20);
+        list.insertAtFirst(30);
+
+        list.inserAtlast(50);
+        list.inserAtlast(60);
+        list.inserAtlast(70);
+
+        list.inserAtOrder(40);  
+    }
+}
