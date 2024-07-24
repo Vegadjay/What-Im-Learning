d15125179c23114bc984e7f5f11eb8719e520703
Jay Vegad <jayvegad10@gmail.com>
2024-07-25 00:31:38 +0530

    intial commit

--- WEB 3/Week-3/Assignment1.js
+++ WEB 3/Week-3/Assignment1.js
@@ -3,19 +3,25 @@ const express = require("express");
 
 const app = express();
 let requestCount = 0;
-app.use(express());
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
-app.get("/user", function (req, res) {
+// Global middleware to count requests
+app.use((req, res, next) => {
   requestCount++;
+  next();
+});
+
+// Parse JSON bodies
+app.use(express.json());
+
+app.get("/user", function (req, res) {
   res.status(200).json({ name: "john" });
 });
 
 app.post("/user", function (req, res) {
-  requestCount++;
   res.status(200).json({ msg: "created dummy user" });
 });
 
