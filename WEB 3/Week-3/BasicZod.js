171e16fd7488994950ac64bc9f88eb0de4f2ccf9
Jay Vegad <jayvegad10@gmail.com>
2024-08-03 12:46:49 +0530

    intial commit

--- WEB 3/Week-3/BasicZod.js
+++ WEB 3/Week-3/BasicZod.js
@@ -1,2 +1,20 @@
-const z = require("zod");
+const zod = require("zod");
+const express = require("express");
+const app = express();
 
+app.use(express.json())
+
+const schema = zod.array(zod.number);
+
+app.post("/do-checkup",(req,res)=>{
+    const kidney = req.body.kidney;
+    const response = schema.safeParse(kidney);
+    res.send({
+      response
+    })
+})
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
