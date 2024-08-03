2b310938eaeea4a7cdc45823bc3aac840fa2cc21
Jay Vegad <jayvegad10@gmail.com>
2024-08-03 10:48:39 +0530

    intial commit

--- WEB 3/Week-3/index2.js
+++ WEB 3/Week-3/index2.js
@@ -1,16 +1,25 @@
+// This is the global catches ex and this 
+
 const express = require("express");
 const app = express();
+// when use body than must use this app.use 
+app.use(express.json())
 
-app.get("/", ((req, res) => {
-    const startTime = Date.now();
-    res.json({
-        msg: "Hi there",
-    });
-    const endTime = Date.now();
-    console.log(`Start time: ${startTime}`);
-    console.log(`End time: ${endTime}`);
-    console.log(`Time taken to process request: ${endTime - startTime} ms`);
-}));
+app.post("/do-checkup",(req,res)=>{
+
+    // require array in kidney data [1,2,3]
+    const kidney = req.body.kidney;
+    const length = kidney.length;
+
+    res.send("Kideny Length is: "  + length)
+})
+
+// This is the global catches and this is send when error is done
+
+app.use(function(err,req,res,next) {
+    res.status(500).send("Wrong Input is done")
+})
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
