2e73441f5c46b291125e5388e4a61812d90d2417
Vegadjay <jayvegad10@gmail.com>
2024-08-11 16:10:34 +0530

    intial commit

--- WEB 3/Week-6/src/App.jsx
+++ WEB 3/Week-6/src/App.jsx
@@ -1,45 +1,53 @@
-import { useContext, useState } from "react";
-import { CountContext } from './componants/Context'
+import { createContext, useContext, useState } from "react";
 import "./App.css";
+const CountContext = createContext();
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-        <CountContext.Provider value={count}>
-        <Count setCount={setCount}/>
-        </CountContext.Provider>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
     </div>
   );
 }
-function Count({setCount}) {
-  return <div>
-    <CounterRender/>
-    <Buttons setCount={setCount}/>
+
+function Count({ setCount }) {
+  return (
+    <div>
+      <CounterRender />
+      <Buttons setCount={setCount} />
     </div>
-  
+  );
 }
 
 function CounterRender() {
   const count = useContext(CountContext);
-  return <div>
-    {count}
-  </div>
+  return <div>{count}</div>;
 }
 
-
-function Buttons({setCount}) {
-  const count = useContext(CountContext)
-  return <div>
-  <button onClick={()=>{
-    setCount(count + 1);  
-  }}>increase</button>
-  <button onClick={()=>{
-    setCount(count - 1);
-  }}>decrese</button>
-  </div>
+function Buttons({ setCount }) {
+  const count = useContext(CountContext);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        decrease
+      </button>
+    </div>
+  );
 }
 
 export default App;
- 
