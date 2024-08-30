2c87ecbb3b4c77efac847141823b9c21e2780d07
Jay Vegad <jayvegad10@gmail.com>
2024-08-30 14:49:09 +0530

    intial commit

--- WEB 3/Week-9/src/App.tsx
+++ WEB 3/Week-9/src/App.tsx
@@ -1,16 +1,14 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [isTrue , setTrue] = useState(false);
   return (
     <>
-      <button>{isTrue ? "True" : "False"}</button>
+      <button onClick={()=> setTrue(!isTrue)}>{isTrue ? "True" : "False"}</button>
     </>
   )
 }
-
-
-
 
 
 export default App
