e7a9cc2b133c762a580b2131a5f32c0be2d489b9
Jay Vegad <jayvegad10@gmail.com>
2024-09-02 22:25:08 +0530

    intial commit

--- WEB 3/TS/b.ts
+++ WEB 3/TS/b.ts
@@ -1,3 +1,5 @@
+import { setTokenSourceMapRange } from "typescript";
+
 type KeyInput = "up" | "down" | "right" | "left";
 
 function doSomething(keyPressed: KeyInput)  {
@@ -9,3 +11,29 @@ function doSomething(keyPressed: KeyInput)  {
 
 doSomething("up");
 doSomething("lakds");
+
+
+// while you doing this instand of doing this make one enum and add all the values into that enum like this
+
+
+enum Direction {
+    Up,
+    Down,
+    Right,
+    Left
+}
+
+// and now do this
+
+function doingSomething( keyPressed : Direction) {
+    if(keyPressed == Direction.Up) {
+        // do something
+    }
+    else if ( keyPressed = Direction.Down) {
+        // do something
+    }
+    // doing this repetedly
+}
+
+doingSomething(Direction.Up);
+doingSomething(Direction.Down);
