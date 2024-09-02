718690611d5a4910510ede973df6e91e1d5ca669
Jay Vegad <jayvegad10@gmail.com>
2024-09-02 22:48:00 +0530

    intial commit

--- WEB 3/TS/b.js
+++ WEB 3/TS/b.js
@@ -1,4 +1,33 @@
 function doSomething(keyPressed) {
-    console.log("Hello World");
+    if (keyPressed == "up") {
+        console.log("up arrow is trigged");
+    }
+    // do something
 }
-doSomething("Hello");
+doSomething("up");
+// doSomething("lakds");
+// while you doing this instand of doing this make one enum and add all the values into that enum like this
+var Direction;
+(function (Direction) {
+    // also you can change the value of this up down and so all like this
+    Direction["Up"] = "up";
+    Direction["Down"] = "down";
+    Direction[Direction["Right"] = void 0] = "Right";
+    Direction[Direction["Left"] = void 0] = "Left";
+    // Up = "up";
+    // Down = "down";
+    // Right = "right";
+    // Left = "left";
+})(Direction || (Direction = {}));
+// and now do this
+function doingSomething(keyPressed) {
+    if (keyPressed == Direction.Up) {
+        // do something
+    }
+    else if (keyPressed = Direction.Down) {
+        // do something
+    }
+    // doing this repetedly
+}
+console.log(Direction.Up);
+console.log(Direction.Down);
--- WEB 3/TS/b.ts
+++ WEB 3/TS/b.ts
@@ -1,5 +1,3 @@
-import { setTokenSourceMapRange } from "typescript";
-
 type KeyInput = "up" | "down" | "right" | "left";
 
 function doSomething(keyPressed: KeyInput)  {
@@ -10,17 +8,24 @@ function doSomething(keyPressed: KeyInput)  {
 }
 
 doSomething("up");
-doSomething("lakds");
+// doSomething("lakds");
 
 
 // while you doing this instand of doing this make one enum and add all the values into that enum like this
 
 
 enum Direction {
-    Up,
-    Down,
-    Right,
-    Left
+    // also you can change the value of this up down and so all like this
+    Up = 'up',
+    Down = 'down',
+    right = "right",
+    left = "left"
+
+    // Up = "up";
+    // Down = "down";
+    // Right = "right";
+    // Left = "left";
+    
 }
 
 // and now do this
@@ -35,5 +40,5 @@ function doingSomething( keyPressed : Direction) {
     // doing this repetedly
 }
 
-doingSomething(Direction.Up);
-doingSomething(Direction.Down);
+console.log(Direction.Up);
+console.log(Direction.Down);
