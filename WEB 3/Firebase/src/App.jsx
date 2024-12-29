82f8876d5232e6e2fe5908af04247871abd7518d
Vegadjay <jayvegad10@gmail.com>
2024-12-29 21:38:02 +0530

    Changes

--- WEB 3/Firebase/src/App.jsx
+++ WEB 3/Firebase/src/App.jsx
@@ -1,13 +1,17 @@
-
-
+import React from 'react';
+import { AuthProvider } from './contexts/AuthContext';
 import Signin from './pages/Signin'
 import './App.css'
+
+// In this code here wrap this into authProvider for track 
 
 function App() {
   const apiUrl = import.meta.env.REACT_APP_FIREBASE_API_KEY;
   return (
     <>
-      <Signin />
+      <AuthProvider>
+        <Signin />
+      </AuthProvider>
     </>
   )
 }
